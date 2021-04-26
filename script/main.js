1459c7913269dafe0d823787a6b249048785bb66
Serj-L <svlepniakov@gmail.com>
2021-04-26 15:11:49 +0300

    add style and base data structure

--- script/main.js
+++ script/main.js
@@ -1,1 +1,10 @@
 'use strict';
+const film1 = {
+    title: 'Jurassic World: Fallen Kingdom',
+    poster: './images/posters/jurassic-world.jpg',
+    rating: 10,
+    realeseDate: '06-06-2020',
+    director: 'J.A. Bayona',
+    budget: '$417,719,760',
+    description: 'Several years after the demise of Jurassic World, a volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar. Claire Dearing, the former park manager and founder of the Dinosaur Protection Group, recruits Owen Grady to help prevent the extinction of the dinosaurs once again.',
+};
